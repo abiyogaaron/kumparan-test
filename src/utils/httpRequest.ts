ec3ed0613a2c44cd6c5c5899d9b9f6af5fb94dda
aaron abiyoga <aaron.sulistio@shopee.com>
2021-08-28 03:33:32 +0700

    create function httpRequest

---
+++ src/utils/httpRequest.ts
@@ -0,0 +1,30 @@
+import axios, {
+  AxiosRequestConfig,
+  AxiosResponse,
+  AxiosError,
+} from 'axios';
+import { IErrorResponse } from '../interface';
+
+type BodyType = object | string | undefined;
+type MethodType = 'GET' | 'POST';
+
+export interface HttpReqCfg extends AxiosRequestConfig {
+  data?: BodyType;
+  method: MethodType;
+  headers?: {[key: string]: string | number}
+}
+
+export function httpReq<OK = any>(url: string, config: HttpReqCfg): Promise<OK> {
+  return new Promise<OK>((resolve, reject) => {
+    axios(url, config)
+      .then((res: AxiosResponse) => {
+        resolve(res.data);
+      })
+      .catch((err: AxiosError) => {
+        const errResp: IErrorResponse = {
+          status: err.response!.status,
+        };
+        reject(errResp);
+      });
+  });
+}

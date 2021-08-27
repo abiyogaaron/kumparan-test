d720bc30ad49206bc2faa457f2d17f7fcf98450f
aaron abiyoga <aaron.sulistio@shopee.com>
2021-08-28 01:09:44 +0700

    add firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import firebase from 'firebase/app';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CONFIG_FIREBASE from './environment';
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(CONFIG_FIREBASE);
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
